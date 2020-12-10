97d91163539e86606a7c3ac47057ac64454aaa1f
qkrwodsbfjq <52436915+qkrwodsbfjq@users.noreply.github.com>
2020-12-10 20:46:45 +0900

    Update server.js

--- server.js
+++ server.js
@@ -5,20 +5,17 @@ const app = express();
 var bodyParser = require("body-parser");
 let path = require('path');
 let server = http.createServer(app);
-
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded({ extended : false }));
-
 request.post("/", function(req, res){
     //let sxv = req.render(sx)
     //let syv = req.render(sy)
     //let exv = req.render(ex)
     //let eyv = req.render(ey)
 });
-
 app.get('/', function (req, response) {
-    let requestUrl = "https://api.odsay.com/v1/api/searchPubTransPath?SX=127.0739547&SY=37.2407701&EX=126.921666&EY=37.1308333&apiKey=D5/cqOgLNnn/iHrOs3rXewYFNPO6KVQvntHKQ/5muGA"
+    let requestUrl = "https://api.odsay.com/v1/api/searchPubTransPath?SX=127.13973017022232&SY=37.47331630562443&EX=127.08011485487083&EY=37.24312944010195&apiKey=D5/cqOgLNnn/iHrOs3rXewYFNPO6KVQvntHKQ/5muGA"
     request.get(requestUrl, (err, res, body) => {
         if (err) {
             console.log(`err => ${err}`)
@@ -41,7 +38,6 @@ app.get('/', function (req, response) {
         }
     })
 });
-
 server.listen('3000', 'localhost', function () {
     console.log('server listen on port:' + server.address().port);
 })
