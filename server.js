15450fdfe623b53a2160c3c15bfde9554a5bee85
qkrwodsbfjq <52436915+qkrwodsbfjq@users.noreply.github.com>
2020-12-10 10:26:25 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,15 +2,23 @@ const express = require('express');
 const request = require('request');
 const http = require('http');
 const app = express();
+var bodyParser = require("body-parser");
 let path = require('path');
 let server = http.createServer(app);
 
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.urlencoded({ extended : false }));
+
+request.post("/", function(req, res){
+    //let sxv = req.render(sx)
+    //let syv = req.render(sy)
+    //let exv = req.render(ex)
+    //let eyv = req.render(ey)
+});
 
 app.get('/', function (req, response) {
-    let requestUrl = "https://api.odsay.com/v1/api/searchPubTransPathT" + 
-"?SX=127.0739547&SY=37.2407701&EX=126.921666&EY=37.1308333&apiKey=D5/cqOgLNnn/iHrOs3rXewYFNPO6KVQvntHKQ/5muGA"
+    let requestUrl = "https://api.odsay.com/v1/api/searchPubTransPath?SX=127.0739547&SY=37.2407701&EX=126.921666&EY=37.1308333&apiKey=D5/cqOgLNnn/iHrOs3rXewYFNPO6KVQvntHKQ/5muGA"
     request.get(requestUrl, (err, res, body) => {
         if (err) {
             console.log(`err => ${err}`)
@@ -21,15 +29,14 @@ app.get('/', function (req, response) {
                 var obj = JSON.parse(result);
                 //console.log(`body data => ${result}`)
                 //console.log('ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ')
-                var timev = (obj)["result"]["path"][0].info.totalTime
-                response.render('index', {time: timev});
-                var payv = (obj)["result"]["path"][0].info.payment
-                response.render('index', {pay: payv});
-                var distancev = (obj)["result"]["path"][0].info.totalDistance
-                response.render('index', {distance: distancev});
-                console.log(timev) // 분단위?
-                console.log(payv)
-                console.log(distancev) // m
+                var time = (obj)["result"]["path"][0].info.totalTime
+                var pay = (obj)["result"]["path"][0].info.payment
+                var distance = (obj)["result"]["path"][0].info.totalDistance
+                response.render('index', {
+                    time: time,
+                    pay: pay,
+                    distance: distance
+                });
             }
         }
     })
